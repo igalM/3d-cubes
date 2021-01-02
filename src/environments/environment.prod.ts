acfe0d201750bb43b4428a5d422f92c24f150f86
igalM <igalmm@gmail.com>
2021-01-02 12:16:30 +0200

    add firestore config to env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCPskXLWKfne0nBGzHx-LrdEy_3y2o1egA",
+    authDomain: "wiply-squares.firebaseapp.com",
+    projectId: "wiply-squares",
+    storageBucket: "wiply-squares.appspot.com",
+    messagingSenderId: "265352291905",
+    appId: "1:265352291905:web:89cfc42733749428a348c0"
+  }
 };
