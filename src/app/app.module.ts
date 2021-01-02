881d0b2f2fa89cd13dc9663b984bf694bf502762
igalM <igalmm@gmail.com>
2021-01-02 12:51:23 +0200

    remove old components and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SquaresItemComponent } from './components/squares-item/squares-item.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { environment } from 'src/environments/environment';
 
 const components = [
   AppComponent,
-  SquaresItemComponent,
   HomepageComponent,
   ErrorComponent
 ];
